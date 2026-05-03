bbd27d0dc0fb8602501ac8413e249053bb47c1cd
MANNAVA DAASARADHI <daasaradhimannava@gmail.com>
2026-05-03 13:01:55 +0530

    fixed the install debug issue

--- core/multiplayer/src/main/kotlin/com/battleship/fleetcommand/core/multiplayer/matchmaking/MatchmakingRepository.kt
+++ core/multiplayer/src/main/kotlin/com/battleship/fleetcommand/core/multiplayer/matchmaking/MatchmakingRepository.kt
@@ -1,13 +1,1 @@
-// FILE: core/domain/src/main/kotlin/com/battleship/fleetcommand/core/domain/multiplayer/MatchmakingError.kt
-package com.battleship.fleetcommand.core.domain.multiplayer
-
-/**
- * Error types for online matchmaking operations.
- * Section 24: sealed class suffixed with Error, noun/adjective subclasses.
- */
-sealed class MatchmakingError {
-    data object RoomNotFound : MatchmakingError()
-    data object RoomFull : MatchmakingError()
-    data object NetworkUnavailable : MatchmakingError()
-    data class Unknown(val cause: Throwable) : MatchmakingError()
-}
+import com.battleship.fleetcommand.core.domain.multiplayer.MatchmakingError
